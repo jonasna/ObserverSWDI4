123e88c55ee1ac107407ae39241a0384df45a9f8
Kristoffer Stampe Villadsen <Villadsen67@hotmail.com>
2018-02-24 15:21:35 +0100

    Imp PorteFolio

---
+++ Observer.Impl/PorteFolio.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Observer.Impl.Interfaces;
+
+
+namespace Observer.Impl
+{
+    public class PorteFolio : IObserver, IPortfolio
+    {
+
+
+
+        private List<Ownstock> MyOwnStock = new List<Ownstock>();
+        public void Update(Subject subject)
+        {
+            var stock = (IStock) subject;
+
+            foreach (var item in MyOwnStock)
+            {
+                if (item.Name.Contains(stock.Navn))
+                {
+                    item.Value = stock.Value;
+                }
+                else
+                {
+                    Console.WriteLine("Stock not in Portefolio");
+                }
+
+            }
+
+        }
+
+        public int TotalStockValue
+        {
+            get
+            {
+                int sum = 0;
+                foreach (var Item in MyOwnStock)
+                {
+                    
+                    sum = Item.Value + sum;
+                    
+                }
+
+                return sum;
+
+
+            }
+        }
+
+        public void AddStock(IStock stock, int amount)
+        {
+            Ownstock mystock = new Ownstock();
+            mystock.Amount = amount;
+            mystock.Name = stock.Navn;
+            mystock.Value = stock.Value;
+           
+            
+
+        }
+
+        
+    }
+
+    
+}
