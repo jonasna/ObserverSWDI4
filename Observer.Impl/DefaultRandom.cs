1277e826bb1a5d9b851e9d6a804b73533ddf4268
Malthe <malthedj@hotmail.com>
2018-02-24 15:13:40 +0100

    Added test class DefaultRandomUnitTests

--- Observer.Impl/DefaultRandom.cs
+++ Observer.Impl/DefaultRandom.cs
@@ -9,12 +9,24 @@ namespace Observer.Impl
 {
     public class DefaultRandom : IRandom
     {
+        private int _result { get; }
+
+        public DefaultRandom(int min, int max)
+        {
+            _result = GenerateRandom(min, max);
+        }
         public int GenerateRandom(int min, int max)
         {
-                int result;
+                int result = 0;
                 Random tempRandom = new Random();
                 result = tempRandom.Next(min, max);
                 return result;
-        } 
+        }
+
+        public int GetResult
+        {
+            get { return _result; }
+
+        }
     }
 }
