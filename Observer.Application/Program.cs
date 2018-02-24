9847b2290b413b2e1735313e41cb2a1104fe1cf3
jonasna <jonasna93@gmail.com>
2018-02-24 16:15:54 +0100

    added application

--- Observer.Application/Program.cs
+++ Observer.Application/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Observer.Impl;
 
 namespace Observer.Application
 {
@@ -10,6 +12,22 @@ namespace Observer.Application
     {
         static void Main(string[] args)
         {
+            var display = new PortefolieDisplay();
+            var portfolio = new PorteFolio(display);
+
+            var AppleStock = new Stock("Apple", 100);
+            var GoogleStock = new Stock("Google", 2000);
+
+            portfolio.AddStock(AppleStock, 1);
+            portfolio.AddStock(GoogleStock, 1000);
+
+            AppleStock.StartRegulate();
+            GoogleStock.StartRegulate();
+
+            while (true)
+            {
+                Thread.Sleep(5000);
+            }
         }
     }
 }
