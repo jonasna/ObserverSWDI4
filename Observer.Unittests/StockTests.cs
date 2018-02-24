fde752a49fe6bf95cf5dfc4fc723fce7f18c4462
Gobbenobber <gobbenobber@gmail.com>
2018-02-24 15:28:49 +0100

    Wrote tests for Stock class.

--- Observer.Unittests/StockTests.cs
+++ Observer.Unittests/StockTests.cs
@@ -16,8 +16,47 @@ namespace Observer.Unittests
         [Test]
         public void StockConstructor_InputArguments_ObjectFieldsMatchArguments()
         {
-            var FakeRegulator = Substitute.For<IRegulator>();
-            var uut = new Stock("ABC", 2, FakeRegulator);
+            var fakeRegulator = Substitute.For<IRegulator>();
+            var uut = new Stock("ABC", 2, fakeRegulator);
+            Assert.That(uut.Value, Is.EqualTo(2));
+            Assert.That(uut.Navn, Is.EqualTo("ABC"));
         }
+
+        [Test]
+        public void StockConstructor_InputArguments_RegulatorStartCalled()
+        {
+            var fakeRegulator = Substitute.For<IRegulator>();
+            var uut = new Stock("ABC", 2, fakeRegulator);
+            uut.StartRegulate();
+            fakeRegulator.Received().Start();
+        }
+
+        [Test]
+        public void StockConstructor_InputArguments_RegulatorStopCalled()
+        {
+            var fakeRegulator = Substitute.For<IRegulator>();
+            var uut = new Stock("ABC", 2, fakeRegulator);
+            uut.StopRegulate();
+            fakeRegulator.Received().Stop();
+        }
+
+        [Test]
+        public void StockConstructor_IncreaseValue_PriceIsEqualToExpected()
+        {
+            var fakeRegulator = Substitute.For<IRegulator>();
+            var uut = new Stock("ABC", 100, fakeRegulator);
+            uut.IncreaseValue();
+            Assert.That(uut.Value, Is.EqualTo(105));
+        }
+
+        [Test]
+        public void StockConstructor_DecreaseValue_PriceIsEqualToExpected()
+        {
+            var fakeRegulator = Substitute.For<IRegulator>();
+            var uut = new Stock("ABC", 100, fakeRegulator);
+            uut.DecreaseValue();
+            Assert.That(uut.Value, Is.EqualTo(95));
+        }
+
     }
 }
