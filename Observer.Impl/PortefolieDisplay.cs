dd638fd9842440c05fd7498f381f090a45fbb98a
Malthe <malthedj@hotmail.com>
2018-02-24 15:29:57 +0100

    Added PortefolieDisplay

---
+++ Observer.Impl/PortefolieDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Observer.Impl.Interfaces;
+
+namespace Observer.Impl
+{
+    public class PortefolieDisplay : IPortfolioDisplay
+    {
+        public void Print(List<Ownstock> mySOwnstocks)
+        {
+
+        }
+    }
+}
