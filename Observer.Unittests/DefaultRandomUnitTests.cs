1edad5a9e66a6be6c6ee7b237d9503fe1c55e048
Malthe <malthedj@hotmail.com>
2018-02-24 14:47:28 +0100

    Added reference to Observer.Impl

--- Observer.Unittests/DefaultRandomUnitTests.cs
+++ Observer.Unittests/DefaultRandomUnitTests.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Observer.Unittests
 {
+    [TestFixture]
     class DefaultRandomUnitTests
     {
+        [Test]
+        void DefaultRandom_ResultIsInRange0_10()
+        {
+            
+        }
     }
 }
