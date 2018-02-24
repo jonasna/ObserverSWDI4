45e088a06249d30a748df241b85e752f4e0bbfb8
Malthe <malthedj@hotmail.com>
2018-02-24 14:38:05 +0100

    Added defaultRandom implementation

--- Observer.Impl/DefaultRandom.cs
+++ Observer.Impl/DefaultRandom.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Observer.Impl.Interfaces;
 
 namespace Observer.Impl
 {
-    class DefaultRandom
+    class DefaultRandom : IRandom
     {
+        public int GenerateRandom(int min, int max)
+        {
+                int result;
+                Random tempRandom = new Random();
+                result = tempRandom.Next(min, max);
+                return result;
+        } 
     }
 }
